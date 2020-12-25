cd6413bc85948997abc75f86b84009a2d0244c49
Ahmet Kutay Karacair <akkaracair@gmail.com>
2020-12-25 19:27:41 +0300

    Login  ve Anasayfa tasarımları yapıldı.

--- screen/Login.js
+++ screen/Login.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, TextInput, StyleSheet, Alert } from "react-native";
+import { View, Text, TouchableOpacity, TextInput, StyleSheet, Alert, SafeAreaView, StatusBar, Image } from "react-native";
+import { ScrollView } from 'react-native-gesture-handler';
 import * as firebase from 'firebase';
 import AsyncStorage from '@react-native-community/async-storage';
 import User from "../components/User";
+const avatar = require('../assets/girisekrean.jpg');
 
 class Login extends Component {
     constructor(props) {
@@ -12,7 +14,7 @@ class Login extends Component {
             Email: "",
             Password: "",
             Username: ""
-            
+
         }
     }
 
@@ -21,13 +23,13 @@ class Login extends Component {
     }
     login(email, password) {
         firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) { });
-        User.Email = email;        
+        User.Email = email;
         User.Password = password;
-        AsyncStorage.setItem("userEmail",email);  
+        AsyncStorage.setItem("userEmail", email);
         this.props.navigation.navigate("Home");
     }
 
-    signup(bool, email, password,username) {
+    signup(bool, email, password, username) {
         if (!(bool)) {
             this.setState({ issign: true });//alertle ayarla emailpasw ve nicki
         }
@@ -44,43 +46,80 @@ class Login extends Component {
     }
     render() {
         return (
-            <View style={{ marginTop: 200, width: 100 + "%", backgroundColor: "black" }}>
-                <View >
-                    <Text style={styles.textstyle}>Email</Text>
-                    <TextInput style={styles.textinputstyle} value={this.state.Email} onChangeText={this.valchange("Email")} />
-                </View>
-                {this.state.issign &&
-                    <View >
-                        <Text style={styles.textstyle}>Username</Text>
-                        <TextInput style={styles.textinputstyle} value={this.state.Username}
-                            onChangeText={this.valchange("Username")} />
+            <SafeAreaView>
+                <ScrollView>
+                    <View style={{ height: StatusBar.currentHeight }} />
+                    <View style={{ height: StatusBar.currentHeight }} />
+                    <View style={{ height: StatusBar.currentHeight }} />
+                    <View style={{ height: StatusBar.currentHeight }} />
+                    <View style={{ height: StatusBar.currentHeight }} />
+
+
+                    <View style={{ alignSelf: 'center' }}>
+                        <View style={styles.profilImage}>
+                            <Image source={avatar} style={styles.image} resizeMode="center" ></Image>
+                        </View>
+                        <Text style={{ alignSelf: "center", height: 30 }}> Welcome To HearMe App </Text>
                     </View>
-                }
-                <View >
-                    <Text style={styles.textstyle}>Password</Text>
-                    <TextInput style={styles.textinputstyle} value={this.state.Password} onChangeText={this.valchange("Password")} />
-                </View>
-                <View style={{ alignItems: "center", width: 100 + "%" }}>
-                    <TouchableOpacity style={styles.loginbutton} onPress={() => this.login(this.state.Email, this.state.Password)}>
-                        <Text style={styles.buttontext}>Login</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={{ alignItems: "center", width: 100 + "%" }}>
-                    <TouchableOpacity style={styles.signupbutton} onPress={() => this.signup(this.state.issign, this.state.Email, this.state.Password,this.state.Username)}>
-                        <Text style={styles.buttontext}>Signup</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+                    <View>
+                        <View >
+
+                            <TextInput style={styles.textinputstyle} placeholder=" Emailinizi Yazınız" value={this.state.Email} onChangeText={this.valchange("Email")} />
+                        </View>
+                        <View style={{ height: 10 }} />
+
+                        {this.state.issign &&
+                            <View >
+                                <TextInput style={styles.textinputstyle} placeholder=" Kullanıcı Adınızı Yazınız" value={this.state.Username}
+                                    onChangeText={this.valchange("Username")} />
+                            </View>
+                        }
+                        <View style={{ height: 10 }} />
+
+                        <View >
+                            <TextInput style={styles.textinputstyle} placeholder=" Şifrenizi Yazınız" value={this.state.Password} onChangeText={this.valchange("Password")} />
+                        </View>
+                        <View style={{ height: 10 }} />
+
+                        {!this.state.issign &&
+                            <View style={{ alignItems: "center", width: 100 + "%" }}>
+                                <TouchableOpacity style={styles.loginbutton} onPress={() => this.login(this.state.Email, this.state.Password)}>
+                                    <Text style={styles.buttontext}>Login</Text>
+                                </TouchableOpacity>
+                            </View>
+                        }
+                        <View style={{ height: 10 }} />
+
+                        <View style={{ alignItems: "center", width: 100 + "%" }}>
+                            <TouchableOpacity style={styles.signupbutton} onPress={() => this.signup(this.state.issign, this.state.Email, this.state.Password, this.state.Username)}>
+                                <Text style={styles.buttontext}>Signup</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+
         )
     }
 }
 
 const styles = StyleSheet.create({
     buttontext: { color: "white", fontSize: 34 },
-    loginbutton: { backgroundColor: "blue", borderRadius: 20, width: 100 + "%", alignItems: "center" },
-    signupbutton: { backgroundColor: "green", borderRadius: 20, width: 100 + "%", alignItems: "center" },
-    textstyle: { color: "white", fontSize: 24 },
-    textinputstyle: { borderRadius: 15, borderColor: "white", color: "white", borderWidth: 1, height: 50, fontSize: 24, alignItems: "center" }
+    loginbutton: { backgroundColor: "orange", borderRadius: 20, width: 100 + "%", alignItems: "center" },
+    signupbutton: { backgroundColor: "gold", borderRadius: 20, width: 100 + "%", alignItems: "center" },
+    textstyle: { color: "black", fontSize: 24, },
+    textinputstyle: { borderRadius: 15, borderColor: "white", color: "black", borderWidth: 1, height: 50, fontSize: 24, alignItems: "center" },
+    image: {
+        flex: 1,
+        width: undefined,
+        height: undefined
+    },
+    profilImage: {
+        width: 250,
+        height: 150,
+        borderRadius: 100,
+        overflow: "hidden"
+    }
 });
 
 export default Login;
--- screen/homeScreen.js
+++ screen/homeScreen.js
@@ -1,8 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, StatusBar, TouchableOpacity,TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar, TouchableOpacity, TextInput, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import 'react-native-gesture-handler';
 import * as firebase from 'firebase';
 import User from "../components/User";
+import { ScrollView } from 'react-native-gesture-handler';
+import { Button } from 'react-native';
+
+
+const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item',
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Second Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d74',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d75',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d76',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d77',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d78',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d79',
+        title: 'Third Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d23',
+        title: 'Third Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d24',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d25',
+        title: 'Third Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d26',
+        title: 'Third Item',
+    },
+
+];
+
+const Item = ({ title }) => (
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+);
+
+const renderItem = ({ item }) => (
+    <Item title={item.title} />
+);
 
 
 export default class HomeScreen extends React.Component {
@@ -10,41 +70,74 @@ export default class HomeScreen extends React.Component {
         super(props);
         this.state = {
             Story: "",
-            textInput:""           
+            textInput: ""
         }
     }
     valchange = key => val => {
         this.setState({ [key]: val })
     }
-    sendstory()
-    {
+    sendstory() {
         //usernamele kaydolunmıcak
-        firebase.database().ref("Stories/" + User.Username).set({ Username: User.Username, Story: this.state.Story, Like:0 })
+        firebase.database().ref("Stories/" + User.Username).set({ Username: User.Username, Story: this.state.Story, Like: 0 })
         this.textInput.clear();
     }
     render() {
         return (
-            <View style={styles.container}>
-                <Text>
-                    Home Sayfasi
-                </Text>
-                <View style={{ alignItems: "center", width: 100 + "%" }}>
-                <TextInput value={this.state.Story} ref={input => { this.textInput = input }} onChangeText={this.valchange("Story")} />
-                    <TouchableOpacity onPress={() => this.sendstory()}>
-                        <Text>Send Story</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+            <SafeAreaView>
+                <ScrollView style={{height:150}}>
+                    <View>
+                        <View style={{ height: 20 }} />
+                        <View style={{ alignItems: "center", width: 100 + "%", }}>
+                            <TextInput placeholder="Hikayenizi Yazınız" style={styles.inputStyle} value={this.state.Story} ref={input => { this.textInput = input }} onChangeText={this.valchange("Story")} />
+                            <TouchableOpacity style={styles.buttonStyle} onPress={() => this.sendstory()}>
+                                <Button title="Send Story" />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
+
+                <ScrollView>
+                    <SafeAreaView style={styles.container}>
+                        <FlatList
+                            data={DATA}
+                            renderItem={renderItem}
+                            keyExtractor={item => item.id}
+
+                        />
+                    </SafeAreaView>
+
+                </ScrollView>
+            </SafeAreaView>
+
+
         );
     }
 }
 
 const styles = StyleSheet.create({
+    buttonStyle: {
+        width: 100,
+        borderRadius: 150
+    },
+    inputStyle: {
+        margin: 15,
+        height: 40,
+        width: 360,
+        borderColor: '#7a42f4',
+        borderWidth: 1,
+        paddingLeft: 6
+    },
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
 });
-
