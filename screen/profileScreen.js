80da1bc75178d6e42b07d8636f3a3929c891970a
Ahmet Kutay Karacair <akkaracair@gmail.com>
2020-12-26 18:51:34 +0300

    Profil ve Arama sayfası düzenlendi.

--- screen/profileScreen.js
+++ screen/profileScreen.js
@@ -1,34 +1,101 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar, FlatList } from 'react-native';
 import 'react-native-gesture-handler';
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
 import { SafeAreaView } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import User from '../components/User';
-const avatar = require('../assets/maxresdefault.jpg');
+
+const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item',
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Second Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d74',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d75',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d76',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d77',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d78',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d79',
+        title: 'Third Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d23',
+        title: 'Third Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d24',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d25',
+        title: 'Third Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d26',
+        title: 'Third Item',
+    },
+
+];
+
+const Item = ({ title }) => (
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+);
+
+const renderItem = ({ item }) => (
+    <Item title={item.title} />
+);
 
 export default class ProfileScreen extends React.Component {
     render() {
         return (
             <SafeAreaView>
                 <ScrollView>
+                    <View style={{ height: 20 }} />
+
                     <View>
                         <Text style={styles.titleBar}>
                             Profil Sayfasi
                         </Text>
                     </View>
+                    <View style={{ height: 20 }} />
+
                     <View style={{ alignSelf: 'center' }}>
-                        <View style={styles.profilImage}>
-                            <Image source={avatar} style={styles.image} resizeMode="center" ></Image>
-                        </View>
                         <View>
                             <Text style={{ alignSelf: "center" }}>{User.Username}</Text>
                         </View>
-
-                        <Button
-                            title="Bisiklete Ters Binmek İster Misin ?"
-                        />
                     </View>
+                    <View style={{ height: 20 }} />
+                    <View style={{alignSelf:'center'}}><Text>Hikayelerim</Text></View>
+                    <View style={{ height: 20 }} />
+
+                </ScrollView>
+
+                <ScrollView>
+                    <SafeAreaView style={styles.container}>
+                        <FlatList
+                            data={DATA}
+                            renderItem={renderItem}
+                            keyExtractor={item => item.id}
+
+                        />
+                    </SafeAreaView>
                 </ScrollView>
             </SafeAreaView>
         );
@@ -53,5 +120,15 @@ const styles = StyleSheet.create({
         marginTop: 24,
         marginHorizontal: 16,
         alignSelf: 'center'
-    }
+    },
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
 });
--- screen/searchScreen.js
+++ screen/searchScreen.js
@@ -1,10 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar, FlatList } from 'react-native';
 import 'react-native-gesture-handler';
 import { SearchBar } from 'react-native-elements';
 import { SafeAreaView } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
+
+const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item',
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Second Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d74',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d75',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d76',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d77',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d78',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d79',
+        title: 'Third Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d23',
+        title: 'Third Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d24',
+        title: 'Third Item',
+    }, {
+        id: '58694a0f-3da1-471f-bd96-145571e29d25',
+        title: 'Third Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d26',
+        title: 'Third Item',
+    },
+
+];
+
+const Item = ({ title }) => (
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+);
+
+const renderItem = ({ item }) => (
+    <Item title={item.title} />
+);
 
 export default class SearchScreen extends React.Component {
     state = {
@@ -29,6 +85,17 @@ export default class SearchScreen extends React.Component {
                         value={search}
                     />
                 </ScrollView>
+
+                <ScrollView>
+                    <SafeAreaView style={styles.container}>
+                        <FlatList
+                            data={DATA}
+                            renderItem={renderItem}
+                            keyExtractor={item => item.id}
+
+                        />
+                    </SafeAreaView>
+                </ScrollView>
             </SafeAreaView>
         );
     }
@@ -40,5 +107,15 @@ const styles = StyleSheet.create({
         borderColor: '#cccccc',
         borderWidth: 0.5,
         borderRadius: 15,
-    }
+    },
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
 });
